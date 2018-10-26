cfeb4bffe4508678192ae986de079ca28ecd7672
Mark Tse <mark.tse@d2l.com>
2018-10-26 16:37:47 -0400

    users - controller init.

---
+++ SmallCommitsWorkshop/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SmallCommitsWorkshop.Models;
+
+namespace SmallCommitsWorkshop.Controllers {
+	[Route( "api/[controller]" )]
+	public class UsersController : Controller {
+		private readonly UsersContext m_usersContext;
+
+		public UsersController( UsersContext usersContext ) {
+			m_usersContext = usersContext;
+
+			if( !m_usersContext.Users.Any() ) {
+				m_usersContext.Users.Add( new User() { Id = 169, UserName = "D2LSupport" } );
+				m_usersContext.Users.Add( new User() { Id = 175, UserName = "user1" } );
+				m_usersContext.SaveChanges();
+			}
+		}
+
+		[HttpGet]
+		public ActionResult<IDictionary<long, string>> GetAll() =>
+			m_usersContext
+				.Users
+				.ToDictionary( user => user.Id, user => user.UserName );
+	}
+}
---
+++ SmallCommitsWorkshopTests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using SmallCommitsWorkshop;
+using SmallCommitsWorkshopTests.Extensions;
+
+namespace SmallCommitsWorkshopTests.Controllers {
+	[TestFixture]
+	internal sealed class UsersControllerTests {
+		private WebApplicationFactory<Startup> m_factory;
+
+		[SetUp]
+		public void SetUp() {
+			m_factory = new WebApplicationFactory<Startup>();
+		}
+
+		[Test]
+		public async Task GetAll_ReturnsUsers() {
+			HttpClient client = m_factory.CreateClient();
+
+			using( HttpResponseMessage response = await client.GetAsync( "/api/users" ) ) {
+				CollectionAssert.AreEquivalent(
+					new Dictionary<long, string>() {
+						{ 169, "D2LSupport" },
+						{ 175, "user1" },
+					}, 
+					await response.Content.ReadAsJsonAsync<IDictionary<long, string>>()
+				);
+			}
+		}
+	}
+}
